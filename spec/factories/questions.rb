eebaff1071eb3ec9016e5a9c2b040fbaa84ef5c8
realvint <victor.malukhin@gmail.com>
2021-11-05 17:03:01 +0300

    feature: User can see list of questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,10 +1,19 @@
 FactoryBot.define do
+  sequence :title do |n|
+    "Question title #{n}"
+  end
+
   factory :question do
     title { 'MyString' }
     body { 'MyText' }
 
     trait :invalid do
       title { nil }
+    end
+
+    trait :for_list do
+      title
+      body { 'Question body' }
     end
   end
 end
