c6fde0c9392801ca1a1316ea32a47867735b5777
Victor Malukhin <victor.malukhin@gmail.com>
2025-01-06 21:14:55 +0300

    Add bootstrap styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  FLASH_NAME = {  success: 'alert-success', notice: 'alert-success',
+                  error: 'alert-danger', alert: 'alert-danger' }.freeze
+
+  def bootstrap_class_for_flash(flash_type)
+    flash = FLASH_NAME[flash_type.to_sym] || flash_type.to_s
+  end
 end
