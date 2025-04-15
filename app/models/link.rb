1b236d9709bd2a81af83e219a1507c667cba1358
Victor Malukhin <victor.malukhin@gmail.com>
2025-04-16 00:15:18 +0300

    Add gist url parse

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,4 +5,8 @@ class Link < ApplicationRecord
 
   validates :name, :url, presence: true
   validates :url, url: true
+
+  def gist?
+    URI.parse(url).host == "gist.github.com"
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Link do
 
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :url }
+
+  describe "#gist?" do
+    it "returns true for a GitHub gist url" do
+      link = described_class.new(url: "https://gist.github.com")
+      expect(link).to be_gist
+    end
+
+    it "returns false for a non-gist url" do
+      link = described_class.new(url: "https://ya.ru")
+      expect(link).not_to be_gist
+    end
+  end
 end
