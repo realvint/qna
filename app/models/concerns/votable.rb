c6543cea1b85f6f805f6ee92d8a0fca973402098
Victor Malukhin <victor.malukhin@gmail.com>
2025-04-17 01:22:14 +0300

    Add votable

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -5,5 +5,18 @@ module Votable
 
   included do
     has_many :votes, as: :votable, dependent: :destroy
+
+    def voting(value:, user:)
+      vote = votes.find_or_initialize_by(user: user)
+
+      return if vote.value == value.to_i
+
+      vote.value += value.to_i
+      vote.save
+    end
+
+    def rating
+      votes.sum(:value)
+    end
   end
 end
