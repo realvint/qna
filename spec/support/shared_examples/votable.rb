c2b92f0b402fd45c07323cfe805b9fc5b1701c8e
Victor Malukhin <victor.malukhin@gmail.com>
2025-04-17 23:42:27 +0300

    Fix spec

--- spec/support/shared_examples/votable.rb
+++ spec/support/shared_examples/votable.rb
@@ -22,16 +22,32 @@ RSpec.shared_examples_for "votable" do
   describe "try to vote +1 again" do
     before { create(:vote, votable: votable, user: user, value: 1) }
 
-    it "does not change vote" do
-      expect { votable.voting(user: user, value: 1) }.not_to change(votable, :rating)
+    it "removes previous +1 vote" do
+      expect { votable.voting(user: user, value: 1) }.to change(votable, :rating).by(-1)
     end
   end
 
   describe "try to vote -1 again" do
     before { create(:vote, votable: votable, user: user, value: -1) }
 
-    it "does not change vote" do
-      expect { votable.voting(user: user, value: -1) }.not_to change(votable, :rating)
+    it "removes previous -1 vote" do
+      expect { votable.voting(user: user, value: -1) }.to change(votable, :rating).by(1)
+    end
+  end
+
+  describe "change vote from +1 to -1" do
+    before { create(:vote, votable: votable, user: user, value: 1) }
+
+    it "changes rating by -2" do
+      expect { votable.voting(user: user, value: -1) }.to change(votable, :rating).by(-2)
+    end
+  end
+
+  describe "change vote from -1 to +1" do
+    before { create(:vote, votable: votable, user: user, value: -1) }
+
+    it "changes rating by +2" do
+      expect { votable.voting(user: user, value: 1) }.to change(votable, :rating).by(2)
     end
   end
 end
