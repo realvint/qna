79ccab9262df0f1195429b03206e83cabecc5cec
Victor Malukhin <victor.malukhin@gmail.com>
2025-04-27 00:53:04 +0300

    Streams only for authorized users

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,12 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    attr_reader :warden
+
+    def connect
+      self.current_user = env["warden"].user || reject_unauthorized_connection
+      @warden = env["warden"] if current_user
+    end
   end
 end
